9a1a3548a245211563076d1d749dca4db2b92705
Alex Bradner <alex@bradner.net>
2012-01-26 16:31:56 +1100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Housemates::Application.config.secret_token = 'd8f69ad98692cb977260ed8222ddb91f1394f569935d5c4002f953738426f52ea60973b228989dc00c0f7eb1114bc3280cc53ffe562532bf37c3397e2981ba37'
